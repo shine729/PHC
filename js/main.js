d1c1dc968a35a24564cc2d9b7a515df271c6d4df
shine729 <66360301+shine729@users.noreply.github.com>
2020-12-01 20:46:17 +0900

    image-slide

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    preventDefaultAnchor();
+});
+
+function preventDefaultAnchor() {
+    $(document).on('click', 'a[href="#"]', function(e) {
+    e.preventDefault();
+    });
+};
+
