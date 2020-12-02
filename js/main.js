e28dbf823451a3058e1765d9463e8436fed8f586
shine729 <66360301+shine729@users.noreply.github.com>
2020-12-03 04:50:39 +0900

    Revert "css:footer"
    This reverts commit 6978a19fad96979fd49612207b147aab08826b3e.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 $(document).ready(function() {
     preventDefaultAnchor();
 });
@@ -9,35 +7,4 @@ function preventDefaultAnchor() {
     e.preventDefault();
     });
 };
-
-
-// 버튼누르면 맨 위로 스크롤
-const arrowUp = document.querySelector('.arrow_up')
-arrowUp.addEventListener('click',() => {
-    scrollIntoView('#visual')
-});
-
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:"smooth"});
-}
-
-
-// #cont1 a태그 누르면 element요소가 나타남 
-$('div.cont1_right ul li a').on('click',function(){
-    var index = $('div.cont1_right ul li').index($(this).parent());
-    // alert(index);
-    $('div.cont1_left ul li').removeClass('visible');
-    $('div.cont1_left ul li:eq('+index+')').addClass('visible');  
-    $('div.cont1_center ul li').removeClass('visible');
-    $('div.cont1_center ul li:eq('+index+')').addClass('visible'); 
-})
-
-$('div.cont1_center ul li').on('click',function(){
-    var index = $('div.cont1_center ul li').index($(this).parent());
-    alert(index);
-    // $('div.cont1_left ul li').removeClass('visible');
-    // $('div.cont1_left ul li:eq('+index+')').addClass('visible');  
-})
-
 
